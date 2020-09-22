0f36a4f3fcc2b91f9fbf85f664d6fda53d7c6ed9
Andrey Varganov <a.varganov@reg.ru>
2020-09-22 11:44:57 +0400

    init

---
+++ yellow.py
@@ -0,0 +1,256 @@
+{
+    'id':'Жёлтая ветка',
+    'x':-149,
+    'y':44,
+    'z':-79,
+    'name':'player MizharkPlay'
+#  'description':' '
+},
+{
+    'id':'Жёлтая ветка',
+    'x':-159,
+    'y':44,
+    'z':1,
+    'name':'no player'
+#  'description':' '
+},
+{
+    'id':'Жёлтая ветка',
+    'x':-253,
+    'y':44,
+    'z':-228,
+    'name':'player _Kaktus_CZ_'
+#  'description':' '
+},
+{
+    'id':'Жёлтая ветка',
+    'x':-308,
+    'y':44,
+    'z':309,
+    'name':'Ферма мобов'
+#  'description':' '
+},
+{
+    'id':'Жёлтая ветка',
+    'x':-334,
+    'y':44,
+    'z':105,
+    'name':'player Torsons'
+#  'description':' '
+},
+{
+    'id':'Жёлтая ветка',
+    'x':-337,
+    'y':44,
+    'z':36,
+    'name':'player JayJay'
+#  'description':' '
+},
+{
+    'id':'Жёлтая ветка',
+    'x':-396,
+    'y':44,
+    'z':51,
+    'name':'player ponchers'
+#  'description':' '
+},
+{
+    'id':'Жёлтая ветка',
+    'x':-517,
+    'y':44,
+    'z':196,
+    'name':'player IL86'
+#  'description':' '
+},
+{
+    'id':'Жёлтая ветка',
+    'x':-527,
+    'y':44,
+    'z':510,
+    'name':'Новогодний остров'
+#  'description':' '
+},
+{
+    'id':'Жёлтая ветка',
+    'x':-597,
+    'y':44,
+    'z':-223,
+    'name':'player _LoveSosa_'
+#  'description':' '
+},
+{
+    'id':'Жёлтая ветка',
+    'x':-636,
+    'y':44,
+    'z':415,
+    'name':'player  Dir19 (loblNemezis)'
+#  'description':' '
+},
+{
+    'id':'Жёлтая ветка',
+    'x':-654,
+    'y':44,
+    'z':-232,
+    'name':'player  VessAY & Kerubifi (BearBlackMag)'
+#  'description':' '
+},
+{
+    'id':'Жёлтая ветка',
+    'x':-696,
+    'y':44,
+    'z':113,
+    'name':'player  oplkill'
+#  'description':' '
+},
+{
+    'id':'Жёлтая ветка',
+    'x':-703,
+    'y':44,
+    'z':-650,
+    'name':'End portal namber 12'
+#  'description':' '
+},
+{
+    'id':'Жёлтая ветка',
+    'x':-714,
+    'y':44,
+    'z':194,
+    'name':'Тогровый центр & комплекс ферм'
+#  'description':' '
+},
+{
+    'id':'Жёлтая ветка',
+    'x':-712,
+    'y':44,
+    'z':-125,
+    'name':'End portal namber 3'
+#  'description':' '
+},
+{
+    'id':'Жёлтая ветка',
+    'x':-739,
+    'y':44,
+    'z':-454,
+    'name':'player GoodDra'
+#  'description':' '
+},
+{
+    'id':'Жёлтая ветка',
+    'x':-751,
+    'y':44,
+    'z':167,
+    'name':'player EUGENlJUS'
+#  'description':' '
+},
+{
+    'id':'Жёлтая ветка',
+    'x':-833,
+    'y':44,
+    'z':174,
+    'name':'player CKOBOPODKA2'
+#  'description':' '
+},
+{
+    'id':'Жёлтая ветка',
+    'x':-849,
+    'y':44,
+    'z':1,
+    'name':'player DamirF777'
+#  'description':' '
+},
+{
+    'id':'Жёлтая ветка',
+    'x':-850,
+    'y':44,
+    'z':512,
+    'name':'End portal namber 13'
+#  'description':' '
+},
+{
+    'id':'Жёлтая ветка',
+    'x':-863,
+    'y':44,
+    'z':407,
+    'name':'player prestig9110 & beJIbIu_WhiTe & InterpoL'
+#  'description':' '
+},
+{
+    'id':'Жёлтая ветка',
+    'x':-874,
+    'y':44,
+    'z':-63,
+    'name':'player _Almazik_'
+#  'description':' '
+},
+{
+    'id':'Жёлтая ветка',
+    'x':-917,
+    'y':44,
+    'z':-661,
+    'name':'Ферма трезубцев & player a_fantasy'
+#  'description':' '
+},
+{
+    'id':'Жёлтая ветка',
+    'x':-971,
+    'y':44,
+    'z':406,
+    'name':'player IwINy (llklxq)'
+#  'description':' '
+},
+{
+    'id':'Жёлтая ветка',
+    'x':-1004,
+    'y':44,
+    'z':-90,
+    'name':'End portal namber 14'
+#  'description':' '
+},
+{
+    'id':'Жёлтая ветка',
+    'x':-1019,
+    'y':44,
+    'z':-6,
+    'name':'player Catogg'
+#  'description':' '
+},
+{
+    'id':'Жёлтая ветка',
+    'x':-1038,
+    'y':44,
+    'z':170,
+    'name':'player Alissi'
+#  'description':' '
+},
+{
+    'id':'Жёлтая ветка',
+    'x':-1038,
+    'y':44,
+    'z':963,
+    'name':'End portal namber 6'
+#  'description':' '
+},
+{
+    'id':'Жёлтая ветка',
+    'x':-1105,
+    'y':44,
+    'z':258,
+    'name':'player CooperLiber'
+#  'description':' '
+},
+{
+    'id':'Жёлтая ветка',
+    'x':-1139,
+    'y':44,
+    'z':866,
+    'name':'player AmuTige'
+#  'description':' '
+},
+{
+    'id':'Жёлтая ветка',
+    'x':-1229,
+    'y':44,
+    'z':-686,
+    'name':'End portal namber 23'
+#  'description':' '
+},
