dd6c1c6e5f1f974ee41a725f1f02967cffa63b28
JuraRusan <69300202+JuraRusan@users.noreply.github.com>
2020-09-30 00:38:03 +0300

    Update pixel_arts #4

--- pixel_arts.py
+++ pixel_arts.py
@@ -4,207 +4,207 @@ pixel_arts = [
             'x':-338,
             'y':58,
             'z':416,
-            'name':'SoftPanda3',
-            'description':'Трисс Меригольд:)))'
+            'name':'Автор: SoftPanda3',
+            'description':'Название: Трисс Меригольд:)))'
         },
         {
             'id':'Пиксель арты',
             'x':-135,
             'y':58,
             'z':518,
-            'name':'Nefz',
-            'description':'Погрызун:)))'
+            'name':'Автор: Nefz',
+            'description':'Название: Погрызун:)))'
         },
         {
             'id':'Пиксель арты',
             'x':122,
             'y':58,
             'z':258,
-            'name':'Nefz',
-            'description':'Любовь...:)))'
+            'name':'Автор: Nefz',
+            'description':'Название: Любовь...:)))'
         },
         {
             'id':'Пиксель арты',
             'x':-137,
             'y':58,
             'z':389,
-            'name':'legushQka',
-            'description':'Карта Короля:)))'
+            'name':'Автор: legushQka',
+            'description':'Название: Карта Короля:)))'
         },
         {
             'id':'Пиксель арты',
             'x':-571,
             'y':58,
             'z':455,
-            'name':'Meghan Markle',
-            'description':'Valhalla && Шальная императица:)))'
+            'name':'Автор: Valhalla Meghan Markle',
+            'description':'Название: Meghan Markle (Шальная императица):)))'
         },
         {
             'id':'Пиксель арты',
             'x':246,
             'y':58,
             'z':256,
-            'name':'joukeecz',
-            'description':'Дедуля:)))'
+            'name':'Автор: joukeecz',
+            'description':'Название: Дедуля:)))'
         },
         {
             'id':'Пиксель арты',
             'x':-2397,
             'y':58,
             'z':-3714,
-            'name':'Kerubifi && VessAY',
-            'description':'Професор...:)))'
+            'name':'Автор: Kerubifi && VessAY',
+            'description':'Название: Професор...:)))'
         },
         {
             'id':'Пиксель арты',
             'x':-2548,
             'y':58,
             'z':-4623,
-            'name':'SoftPanda3',
-            'description':'"ВанКорг":)))'
+            'name':'Автор: SoftPanda3',
+            'description':'Название: "ВанКорг":)))'
         },
         {
             'id':'Пиксель арты',
             'x':2190,
             'y':58,
             'z':-4871,
-            'name':'BlackPirat',
-            'description':'Скажем нет кубизму:)))'
+            'name':'Автор: BlackPirat',
+            'description':'Название: Скажем нет кубизму:)))'
         },
         {
             'id':'Пиксель арты',
             'x':-5,
             'y':58,
             'z':260,
-            'name':'Immortal_life',
-            'description':'Чеширский кот:)))'
+            'name':'Автор: Immortal_life',
+            'description':'Название: Чеширский кот:)))'
         },
         {
             'id':'Пиксель арты',
             'x':-135,
             'y':58,
             'z':260,
-            'name':'MinorSpaсe',
-            'description':'Планета:)))'
+            'name':'Автор: MinorSpaсe',
+            'description':'Название: Планета:)))'
         },
         {
             'id':'Пиксель арты',
             'x':-5,
             'y':58,
             'z':380,
-            'name':'MinorSpace',
-            'description':'Космонавт:)))'
+            'name':'Автор: MinorSpace',
+            'description':'Название: Космонавт:)))'
         },
         {
             'id':'Пиксель арты',
             'x':-5,
             'y':58,
             'z':510,
-            'name':'VladykaSirius',
-            'description':'???:)))'
+            'name':'Автор: VladykaSirius',
+            'description':'Название: ???:)))'
         },
         {
             'id':'Пиксель арты',
             'x':-5,
             'y':58,
             'z':650,
-            'name':'VladykaSirius',
-            'description':'Сublay:)))'
+            'name':'Автор: VladykaSirius',
+            'description':'Название: Сublay:)))'
         },
         {
             'id':'Пиксель арты',
             'x':170,
             'y':58,
             'z':390,
-            'name':'lodachka',
-            'description':'Coffin dance meme:)))'
+            'name':'Автор: lodachka',
+            'description':'Название: Coffin dance meme:)))'
         },
         {
             'id':'Пиксель арты',
             'x':190,
             'y':58,
             'z':570,
-            'name':'beJIbIu_WhiTe && prestig9110',
-            'description':'Инь и ян:)))'
+            'name':'Автор: beJIbIu_WhiTe && prestig9110',
+            'description':'Название: Инь и ян:)))'
         },
         {
             'id':'Пиксель арты',
             'x':-2400,
             'y':58,
             'z':-5000,
-            'name':'SoftPanda3',
-            'description':'Хлоя:)))'
+            'name':'Автор: SoftPanda3',
+            'description':'Название: Хлоя:)))'
         },
         {
             'id':'Пиксель арты',
             'x':-2270,
             'y':58,
             'z':-4640,
-            'name':'SoftPanda3',
-            'description':'Картина девушки часть №1:)))'
+            'name':'Автор: SoftPanda3',
+            'description':'Название: Картина девушки часть №1:)))'
         },
         {
             'id':'Пиксель арты',
             'x':-2270,
             'y':58,
             'z':-4380,
-            'name':'SoftPanda3',
-            'description':'Картина девушки часть №2:)))'
+            'name':'Автор: SoftPanda3',
+            'description':'Название: Картина девушки часть №2:)))'
         },
         {
             'id':'Пиксель арты',
             'x':-2000,
             'y':58,
             'z':-4640,
-            'name':'SoftPanda3',
-            'description':'Картина девушки часть №3:)))'
+            'name':'Автор: SoftPanda3',
+            'description':'Название: Картина девушки часть №3:)))'
         },
         {
             'id':'Пиксель арты',
             'x':-2000,
             'y':58,
             'z':-4380,
-            'name':'SoftPanda3',
-            'description':'Картина девушки часть №4:)))'
+            'name':'Автор: SoftPanda3',
+            'description':'Название: Картина девушки часть №4:)))'
         },
         {
             'id':'Пиксель арты',
             'x':-2300,
             'y':58,
             'z':4300,
-            'name':'Hutas && MinorSpace',
-            'description':'Мы следим за тобой, ничтожество часть №1:)))'
+            'name':'Автор: Hutas && MinorSpace',
+            'description':'Название: Мы следим за тобой, ничтожество часть №1:)))'
         },
         {
             'id':'Пиксель арты',
             'x':-2650,
             'y':58,
             'z':4050,
-            'name':'Hutas && MinorSpace',
-            'description':'Мы следим за тобой, ничтожество часть №2:)))'
+            'name':'Автор: Hutas && MinorSpace',
+            'description':'Название: Мы следим за тобой, ничтожество часть №2:)))'
         },
         {
             'id':'Пиксель арты',
             'x':-2800,
             'y':58,
             'z':3950,
-            'name':'Hutas && MinorSpace',
-            'description':'Мы следим за тобой, ничтожество часть №3:)))'
+            'name':'Автор: Hutas && MinorSpace',
+            'description':'Название: Мы следим за тобой, ничтожество часть №3:)))'
         },
         {
             'id':'Пиксель арты',
             'x':-3000,
             'y':58,
             'z':3550,
-            'name':'Hutas && MinorSpace',
-            'description':'Мы следим за тобой, ничтожество часть №4:)))'
+            'name':'Автор: Hutas && MinorSpace',
+            'description':'Название: Мы следим за тобой, ничтожество часть №4:)))'
         },
         {
             'id':'Пиксель арты',
             'x':-338,
             'y':58,
             'z':416,
-            'name':'CublayCraft',
-            'description':'Газета CublayCraft:)))'
+            'name':'Автор: CublayCraft',
+            'description':'Название: Газета CublayCraft:)))'
         },
 ]
